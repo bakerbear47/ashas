f88dcbfe545aa2b4b77076cb155ce6a66e914112
bakerbear47 <bakerbear47@hotmail.com>
2020-04-27 02:18:33 +0200

    with switch statement

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ void setup() {
   WiFi.begin(ssid, password);
   pinMode(pinX, INPUT);
   pinMode(pinY, INPUT);
-  pinMode(button, INPUT_PULLUP);
+  pinMode(buttonClick, INPUT_PULLUP);
   pinMode(potPin,INPUT);
   prevX = analogRead(pinX);
   prevY = analogRead(pinY); 
@@ -34,13 +34,13 @@ void direction(int horizontal, int vertical){// heres a class for the joystick w
   if(horizontal == 0){
     udp.broadcastTo("moveleft", 7000);
     Serial.println("left");
-  } else if(horizontal ==4095){
+  } if(horizontal ==4095){
     udp.broadcastTo("moveright", 7000);
     Serial.println("Right");
-  } else if(vertical == 0){
+  } if(vertical == 0){
     udp.broadcastTo("moveup", 7000);
     Serial.println("Up");
-  }else if(vertical == 4095){
+  } if(vertical == 4095){
     udp.broadcastTo("movedown", 7000);
     Serial.println("Down");
   } else{
@@ -50,14 +50,41 @@ void direction(int horizontal, int vertical){// heres a class for the joystick w
 }
 
 void speed(int Speed){ //heres a class over the potentiometer which defines the speed through 4 stages.
-  if (Speed < 1000){
-    udp.broadcastTo("1", 7000);
-  } else if (Speed >1000 || Speed < 2000){
-    udp.broadcastTo("4", 7000);
-  }else if(Speed > 2000 || Speed < 3000){ 
-    udp.broadcastTo("7", 7000);
-  }else if(Speed > 3000 || Speed <= 4095){ 
-    udp.broadcastTo("9", 7000);
+  /*if (Speed < 1000){
+    udp.broadcastTo("speed 1", 7000);
+  }  (Speed >1000 || Speed < 2000){
+    udp.broadcastTo("speed 4", 7000);
+  } (Speed > 2000 || Speed < 3000){ 
+    udp.broadcastTo("speed 7", 7000);
+  }Speed > 3000 || Speed <= 4095){ 
+    udp.broadcastTo("speed 9", 7000);
+  }*/
+  switch(Speed){
+      case 1:
+          udp.broadcastTo("1", 7000);
+          Serial.println("speed1");
+              break;
+      case 2:
+          udp.broadcastTo("2", 7000);
+          Serial.println("speed2");
+      break;
+      case 3:
+          udp.broadcastTo("3", 7000);
+          Serial.println("speed2");
+      break;
+      case 4:
+          udp.broadcastTo("4", 7000);
+          Serial.println("speed2");
+      break;
+      
+      /*case 2001 ... 3000:
+          udp.broadcastTo("speed 7", 7000);
+          Serial.println("speed3");
+      
+      case 3001 ... 4095:
+          udp.broadcastTo("speed 9", 7000);
+          Serial.println("speed4");
+      */
   }
 }
 
@@ -73,8 +100,22 @@ void loop() {
     udp.broadcastTo("init 9 9", 7000);
     Serial.println("Clicked");
   }
-  speed(potVal = analogRead(potPin));// here it reads the input from the potentiometer and puts it in the speed class
+  potVal = analogRead(potPin);// here it reads the input from the potentiometer and puts it in the speed class
   
-  Serial.println(potVal);
+  if(potVal < 1000){
+      speed(1);
+  }
+  if(potVal > 1000 || potVal < 2000){
+      speed(2);
+  }
+  if(potVal > 2000 || potVal < 3000){
+      speed(3);
+  }
+  if(potVal > 3000 || potVal <= 4095){
+      speed(4);
+  }
+
+
+ // Serial.println(potVal);
 
 }
